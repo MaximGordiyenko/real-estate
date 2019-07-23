6d65100b5dafd6ac445fd3229e38c58260f130c8
Maksym Gordiyenko <maxim.gordiyenko@gmail.com>
2019-07-23 16:11:10 +0300

    problem with path image

--- src/Components/Card/CardView.js
+++ src/Components/Card/CardView.js
@@ -3,13 +3,14 @@ import React from 'react';
 import movies from "../../api/movies";
 
 const CardView = ({match}) => {
-    const matchId = Number(Object.values(match.params.id));
     const itemList = movies.map(e => e);
     let found_object = null;
+    const matchId = Number(Object.values(match.params.id));
+
     itemList.forEach((item) => {
         if (item.id === matchId) {
-            found_object = item;
             console.log(found_object.image); //  poster/SPECTACULAR-OCEANFRONT.jpg
+            found_object = item;
         }
     });
     if (found_object === null) {
@@ -17,12 +18,12 @@ const CardView = ({match}) => {
     }
     return (
       <div>
-          <h2>{found_object.title}</h2>
+          <p>{found_object.releaseDate}</p>
           <p>{found_object.description}</p>
-          <img src={found_object.image} alt={found_object.title}/>
+          <h2>{found_object.title}</h2>
           <p>{found_object.price}</p>
           <p>{found_object.location}</p>
-          <p>{found_object.releaseDate}</p>
+          <img src={found_object.image} alt={found_object.title}/>
       </div>
     )
 };
