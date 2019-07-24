e4c71d8e816bb213202503f44717e0d67cc3d8da
Maksym Gordiyenko <maxim.gordiyenko@gmail.com>
2019-07-25 00:13:11 +0300

    fix location (window.location.origin)

--- src/Components/Card/CardView.js
+++ src/Components/Card/CardView.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import style from "../../css/Card.module.css";
+import style from "../../css/CardView.module.css";
 import movies from "../../api/movies";
 
 const CardView = ({match}) => {
@@ -16,14 +16,22 @@ const CardView = ({match}) => {
     }
     const {releaseDate, description, title, price, location, image} = found_object;
     return (
-      <>
-          <p>{releaseDate}</p>
-          <p>{description}</p>
-          <h2>{title}</h2>
-          <p>{price}</p>
-          <p>{location}</p>
-          <img src={image} alt={found_object.title}/>
-      </>
+      <div className={style.wrapper}>
+          <div className={style.container}>
+              <h1 className={style.title}>{title}</h1>
+
+              <div>
+                  <img className={style.image}
+                       src={`${window.location.origin}/${image}`}
+                       alt={found_object.title}/>
+                  <p> {description} Coast without TAX: <span>{price}. </span>
+                                    Object was release: <span>{releaseDate}</span>
+                  </p>
+              </div>
+
+              <p>{location}</p>
+          </div>
+      </div>
     )
 };
 
